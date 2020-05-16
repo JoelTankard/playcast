d2abeb34cf074f6ca06491c9d423b2f569105c23
JoelTankard <hello@joeltankard.com>
2020-05-16 17:17:03 +1000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <div id="nav" class="absolute p-4">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home flex justify-center items-center h-screen">
+    <div class="text-center">
+          <img class="inline-block w-32" alt="Vue logo" src="../assets/logo.png">
+          <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
